bd45d3b163e5d9f0e3c9036c77c2d1cd5d835c1a
juwoncaleb <calebomojuwon@gmail.com>
2022-02-17 18:03:08 +0100

    new

--- src/app/store.js
+++
@@ -1,7 +0,0 @@
-import { createStore } from "redux";
-import rootReducer from "./rootReducers";
-import { persistStore } from "redux-persist";
-
-export const store = createStore(rootReducer);
-
-export const persistor = persistStore(store);
